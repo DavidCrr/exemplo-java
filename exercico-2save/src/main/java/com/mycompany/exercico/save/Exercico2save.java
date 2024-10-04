4e3e58203c9bb53cda3b8c463c0b74bada99a607
DavidCrr <david.contato62@gmail.com>
2024-10-04 20:28:17 -0300

    começo do projeto

---
+++ exercico-2save/src/main/java/com/mycompany/exercico/save/Exercico2save.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercico.save;
+
+/**
+ *
+ * @author ThinkPad T470
+ */
+public class Exercico2save {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
